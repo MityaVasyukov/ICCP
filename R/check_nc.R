0106fb86458b0b4c0ecb35f77af58102130036d5
Mitya Kletzerman <tipa2128506@gmail.com>
2024-08-11 15:28:25 +0300

    Update

--- R/check_nc.R
+++ R/check_nc.R
@@ -8,11 +8,16 @@
 #' @import RNetCDF
 #' @examples
 #' filename <- "israel_caves-2024.nc"
-#' check_netcdf_file(filename)
+#' check_nc(filename)
 
-check_netcdf_file <- function(file_name = "israel_caves-2024.nc") {
+check_nc <- function(file_name = "israel_caves-2024.nc") {
 
-  file_path <- system.file("extdata", file_name, package = "ICCP")
+  # Check if the user specified an nc file
+  if (file_name == "israel_caves-2024.nc") {
+    file_path <- system.file("extdata", file_name, package = "ICCP")
+  } else {
+    file_path <- file_name
+  }
 
   nc <- tryCatch(RNetCDF::open.nc(file_path), error = function(e) NULL)
 
@@ -22,7 +27,7 @@ check_netcdf_file <- function(file_name = "israel_caves-2024.nc") {
     return(TRUE)
     RNetCDF::close.nc(nc)
   } else {
+    cat("Failed to open netcdf file.\n")
     return(FALSE)
-    stop("Failed to open netcdf file.")
   }
 }
--- tests/testthat/test-iccp.R
+++ tests/testthat/test-iccp.R
@@ -1,3 +1,3 @@
-test_that("check_netcdf_file() returns TRUE when netcdf is ok", {
-  expect_true(check_netcdf_file("israel_caves-2024.nc"))
+test_that("check_nc() returns TRUE when netcdf is ok", {
+  expect_true(check_nc("israel_caves-2024.nc"))
 })
